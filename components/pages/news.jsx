f0fa1242aa6800707b67655432fdd93e46f77cdc
Nishant Patel <nishantpatel197322@gmail.com>
2024-08-10 11:49:30 +0530

    nishant

--- components/pages/news.jsx
+++ components/pages/news.jsx
@@ -1,8 +1,24 @@
+import Header from '../header.jsx';
+import Image from 'next/image'
+import Card from '../Card.js';
+
+
  const News=()=>{
     return(
         <>
-        
-        <h1>Hi</h1>
+        <div className="main">
+        <Header/>
+       <div className="news">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+       </div>
+        </div>
         </>
     )
 }
---
+++ components/pages/price.jsx
@@ -0,0 +1,8 @@
+ import Header from "../header"
+ const price=()=>{
+    return(
+        <>
+        <Header/>
+        </>
+    )
+ }
